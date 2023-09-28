ae2816ed8dfa3c1e6aeceab5649722f3a1f45a19
phanhuyhiep2k3@gmail.com <phanhuyhiep2k3@gmail.com>
2023-09-28 12:39:39 +0700

    ui footer

--- FE-HYYERX/src/app/components/stack/header/header.component.tsx
+++ FE-HYYERX/src/app/components/stack/header/header.component.tsx
@@ -8,51 +8,51 @@ const Header = () => {
       <div className='bg-[#f5f5f5]'>
           <div className='flex justify-between w-[1140px] m-auto pb-2 pt-2 text-xs'>
             <div className=''>
-            <a href="#"><h2 className='text-[#BF0000]'>Earn Rewards</h2></a>
+            <a href="#"><h2 className='text-[#BF0000] font-semibold italic'>Earn Rewards</h2></a>
             </div>
             <div className='flex'>
-              <div className='px-3'>English</div>
-              <a href="#" className='px-3'>Gift Cards</a>
-              <a href="#" className='px-3'>Blog</a>
-              <a href="#" className='px-3'>Help</a>
+              <div className='px-3 text-[#595959] font-semibold'>English</div>
+              <a href="#" className='px-3 text-[#595959] font-semibold'>Gift Cards</a>
+              <a href="#" className='px-3 text-[#595959] font-semibold'>Blog</a>
+              <a href="#" className='px-3 text-[#595959] font-semibold  '>Help</a>
               </div>
             </div>
       </div>
       <div className='flex justify-between m-auto w-[1140px] mt-2'>
         <div className='flex items-center'>
           <img src="https://res.cloudinary.com/dpfndtcya/image/upload/v1695868388/Screenshot_2023-09-28_093249_lfrm2d.png" alt="" />
-          <div className='flex '>
+          <div className='flex mx-10'>
             <input
                 type='text'
                 className='px-8 w-[448px] border border-[#D8D8D8] py-3 text-[11px]'
                 placeholder='Search by title, author, series or ISBN'
             />
-          </div>
-          <button className='bg-black text-white px-[10px] py-[10px]'>
+            <button className='bg-black text-white px-[10px] py-[10px]'>
               <BsSearch size={22}/>
             </button>
+          </div>
         </div>
         <div className='flex items-center'>
     
-            <button className='px-3 '>
-              <BsHeart size={28} className='m-auto mt-3'/>
+            <button className='px-3 text-[#595959] font-semibold'>
+              <BsHeart size={28} className='m-auto mt-3 ' />
               <a href="" className='text-xs'>Wishlist</a>  
             </button>
 
-            <button className='px-3'>
+            <button className='px-3 text-[#595959] font-semibold'>
               <GiShoppingCart size={38}/>
               <a href="" className='text-xs'>Cart</a>
             </button>           
         
           <div className='px-3 pt-3'>
             <button className='bg-[#BF0000] py-3 px-5 text-sm font-semibold text-white'>Create account</button>
-            <p className='text-xs mt-2'>Have an account? <a href="" className='text-[#BF0000] '>Sign in</a></p>
+            <p className='text-xs mt-2 text-[#595959] font-semibold'>Have an account? <a href="" className='text-[#BF0000] '>Sign in</a></p>
           </div>
 
         </div>
       </div>
       <div className='m-auto w-[1140px] mt-2'>
-        <ul className=' w-[300px] flex justify-between'>
+        <ul className=' w-[300px] flex justify-between italic'>
           <li><a href="#">Home</a></li>
           <li><a href="#">Product</a></li>
           <li><a href="#">Contact</a></li>
