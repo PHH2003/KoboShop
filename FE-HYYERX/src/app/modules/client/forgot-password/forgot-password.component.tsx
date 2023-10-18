861d6d75141b04842d5d89c757e799fd670ab731
phanhuyhiep2k3@gmail.com <phanhuyhiep2k3@gmail.com>
2023-10-18 19:24:29 +0700

    emailjs for contact

--- FE-HYYERX/src/app/modules/client/forgot-password/forgot-password.component.tsx
+++ FE-HYYERX/src/app/modules/client/forgot-password/forgot-password.component.tsx
@@ -1,53 +1,59 @@
-import { css } from "@emotion/react"
-import ButtonComponent from "~/app/components/parts/button/button.component"
+import { css } from '@emotion/react'
+import { Link } from 'react-router-dom'
+import ButtonComponent from '~/app/components/parts/button/button.component'
 
 const ForgotPasswordComponent = () => {
-    return (
-        <div className="relative h-[90vh]" css={cssRegister}>
-            <div className="">
-                <img src="https://authorize.kobo.com/Images/prism_large.png" alt="" className='w-full' />
-            </div>
-            <div className="absolute top-[20px] left-[40%] ">
-                <img className="mx-auto" src="https://res.cloudinary.com/dpfndtcya/image/upload/t_logo-kobo-web-admin/v1696241284/rakuten-kobo1-removebg-preview_efmks8.png" alt="" />
-                <div className='bg-white border p-4 rounded w-[300px] m-auto'>
-                    <a href="/login" className="text-gray-700 font-semibold hover:text-red-700 float-right underline">Login Kobo account</a>
-                    <div className='mt-10'>
-                        <h2 className='text-center'>Forgot Password</h2>
-                    </div>
-                    <input type="email" name="" id="" placeholder='Email' />
-                    
-                    <p className='text-[12px] text-gray-800 font-semibold mt-3'>By continuing you confirm that you agree to the Terms of Use and confirm that you have read the Privacy Policy, updated August 15, 2023</p>
+  return (
+    <div className='relative h-[90vh]' css={cssRegister}>
+      <div className=''>
+        <img src='https://authorize.kobo.com/Images/prism_large.png' alt='' className='w-full' />
+      </div>
+      <div className='absolute top-[20px] left-[40%] '>
+        <img
+          className='mx-auto'
+          src='https://res.cloudinary.com/dpfndtcya/image/upload/t_logo-kobo-web-admin/v1696241284/rakuten-kobo1-removebg-preview_efmks8.png'
+          alt=''
+        />
+        <div className='bg-white border p-4 rounded w-[300px] m-auto'>
+          <Link to={'/login'}>
+            <a className='text-gray-700 font-semibold hover:text-red-700 float-right underline'>Login</a>
+          </Link>
 
-                    <ButtonComponent handleColor title={"Get Password"} className="w-full mt-3 hover:bg-[#595959]" />
-                </div>
-                <div className="w-[300px] m-auto text-center mt-4 text-[13px] font-bold text-gray-900">
-                    Terms of Use Privacy Policy
-                    2023 Rakuten Kobo Inc.
+          <div className='mt-10'>
+            <h2 className='text-center'>Forgot Password</h2>
+          </div>
+          <input type='email' name='' id='' placeholder='Email' />
 
-                    This site is protected by hCaptcha and its Privacy Policy and Terms of Service apply.
-                </div>
-            </div>
+          <p className='text-[12px] text-gray-800 font-semibold mt-3'>
+            By continuing you confirm that you agree to the Terms of Use and confirm that you have read the Privacy
+            Policy, updated August 15, 2023
+          </p>
 
-
+          <ButtonComponent handleColor title={'Get Password'} className='w-full mt-3 hover:bg-[#595959]' />
         </div>
-
-    )
+        <div className='w-[300px] m-auto text-center mt-4 text-[13px] font-bold text-gray-900'>
+          Terms of Use Privacy Policy 2023 Rakuten Kobo Inc. This site is protected by hCaptcha and its Privacy Policy
+          and Terms of Service apply.
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ForgotPasswordComponent
 
 export const cssRegister = css`
-input{
+  input {
     border: 1px solid;
-    padding:5px;
-    margin-top:15px;
-    border-radius:5px;
-    width:100%;
-}
-h2{
+    padding: 5px;
+    margin-top: 15px;
+    border-radius: 5px;
+    width: 100%;
+  }
+  h2 {
     font-size: 2.2rem;
-    font-family: "Trebuchet MS",Trebuchet,Arial,Helvetica,sans-serif;
+    font-family: 'Trebuchet MS', Trebuchet, Arial, Helvetica, sans-serif;
     color: #000;
     font-weight: 400;
-}
+  }
 `
--- FE-HYYERX/src/app/modules/client/help/components/form/form.component.tsx
+++ FE-HYYERX/src/app/modules/client/help/components/form/form.component.tsx
@@ -1,25 +1,70 @@
-import ButtonComponent from "~/app/components/parts/button/button.component"
+import ButtonComponent from '~/app/components/parts/button/button.component'
+import emailjs from '@emailjs/browser'
+import { useState } from 'react'
+import toast from 'react-hot-toast'
 
 const FormComponent = () => {
+
+  const [values, setValues] = useState({
+    fullname: '',
+    email: '',
+    message: ''
+  })
+  const handleValue = (e: any) => {
+    setValues((values)=>({
+      ...values,
+      [e.target.name]: e.target.value
+    }))
+    
+  }
+
+  const sendEmail = (e: any) => {
+    e.preventDefault()
+
+    emailjs.send(
+      'service_8emck25',
+       'template_vshb50c',
+       values,
+       'ke1yKLn-ThEsICxpr').then(
+      (result) => {
+        toast.success("Feedback sent successfully")
+      },
+      (error) => {
+        toast.success("Response has not been sent")
+      }
+    )
+  }
   return (
-    <div className="ml-11">
-        <h2 className="font-semibold font-mono">Please make sure to provide correct email so that we can contact you for support.</h2>
-        <div className="my-3">
-            <p className="flex">Your Name <p className="text-red-600 ml-2">*</p></p>
-            <input type="text" className="mt-2 w-60 border border-gray-400 rounded text-lg" />
+    <div>
+      <div className='ml-11' >
+        <h2 className='font-semibold font-mono'>
+          Please make sure to provide correct email so that we can contact you for support.
+        </h2>
+        <div className='my-3'>
+          <p className='flex'>
+            Your Name <p className='text-red-600 ml-2'>*</p>
+          </p>
+          <input name='fullname' onChange={handleValue} type='text' className='mt-2 w-60 border border-gray-400 rounded text-lg' />
         </div>
-        <div className="my-3">
-            <p className="flex">Your Email <p className="text-red-600 ml-2">*</p></p>
-            <input type="text" className="mt-2 w-60 border border-gray-400 rounded text-lg" />
+        <div className='my-3'>
+          <p className='flex'>
+            Your Email <p className='text-red-600 ml-2'>*</p>
+          </p>
+          <input name='email' onChange={handleValue} type='text' className='mt-2 w-60 border border-gray-400 rounded text-lg' />
         </div>
         <div>
-            <p className="flex">Your Message, Question, Comment, or Testimonial <p className="text-red-600 ml-2">*</p></p>
-            <textarea  className="mt-2 w-[450px] h-[200px] border border-gray-400 rounded text-lg  text-start" />
+          <p className='flex'>
+            Your Message, Question, Comment, or Testimonial <p className='text-red-600 ml-2'>*</p>
+          </p>
+          <textarea name='message' onChange={handleValue} className='mt-2 w-[450px] h-[200px] border border-gray-400 rounded text-lg  text-start' />
         </div>
-        <div className="my-3">
-        <ButtonComponent handleColor title={"Submit"} className="rounded hover:bg-[#595959] mt-3 w-[100px]"/>
-        <a href="#" className="ml-4 underline font-semibold">Cancel</a>
+        <div className='my-3'>
+          <ButtonComponent  onClick={sendEmail} handleColor title={'Submit'} className='rounded hover:bg-[#595959] mt-3 w-[100px]' />
+          <a href='#' className='ml-4 underline font-semibold'>
+            Cancel
+          </a>
         </div>
+      </div>
     </div>
   )
 }
