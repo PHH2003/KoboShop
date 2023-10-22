eec9709063f5b14fd987312a43a4aa078b0fd9be
phanhuyhiep2k3@gmail.com <phanhuyhiep2k3@gmail.com>
2023-10-23 06:52:29 +0700

    logic search product and total cary

--- FE-HYYERX/src/app/modules/client/cart/components/buy-cart/buy-cart.component.tsx
+++ FE-HYYERX/src/app/modules/client/cart/components/buy-cart/buy-cart.component.tsx
@@ -1,12 +1,24 @@
 import { css } from "@emotion/react"
 import { Link } from "react-router-dom"
 import ButtonComponent from "~/app/components/parts/button/button.component"
+import { useCartRedux } from "../../../redux/hook/useCartReducer"
+import { useEffect } from "react"
 const BuyCart = () => {
+    const   {data: {carts}, actions} = useCartRedux()
+    useEffect(()=> {
+        actions.getAllCarts()
+    },[])
+    const total = carts.reduce((accumulator: any, cartItem: any)=> {
+        const productPrice = cartItem.product.newPrice;
+        const quantity = cartItem.quantity;
+        const itemTotal = productPrice * quantity
+        return accumulator + itemTotal
+    }, 0)
     return (
         <div className="w-[25%] border p-4 h-[150px] mt-6" css={cssBuy}>
             <div className="flex justify-between py-2 text-[#595959]">
                 <p className="font-mono">Total amount:</p>
-                <span className="font-mono">$14,9</span>
+                <span className="font-mono">${total}</span>
             </div>
                 <div className="py-6">
                     <Link to={'/checkout'}>
--- FE-HYYERX/src/app/modules/client/product/main-product/main-product.component.tsx
+++ FE-HYYERX/src/app/modules/client/product/main-product/main-product.component.tsx
@@ -1,25 +1,31 @@
-import { useProductRedux } from "../../redux/hook/useProductReducer"
-import { useEffect } from "react"
-import { Link } from "react-router-dom"
-import Itemproduct from '~/app/components/parts/item-product/item-product.component';
+import { useProductRedux } from '../../redux/hook/useProductReducer'
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Itemproduct from '~/app/components/parts/item-product/item-product.component'
 
 const MainProductComponent = () => {
-    const {
-        data: { products },
-        actions
-    } = useProductRedux() 
-    useEffect(()=> {
-        actions.getAllProducts()
-    },[])
+  const keyWords = new URLSearchParams(location.search).get('q')
+  const {
+    data: { products },
+    actions
+  } = useProductRedux()
+  const filteredProducts = keyWords
+    ? products.filter((item: any) => item?.name.toLowerCase().includes(keyWords.toLowerCase()))
+    : products
+  useEffect(() => {
+    actions.getAllProducts()
+  }, [keyWords])
   return (
-    <div>
-        <div className="grid grid-cols-5 gap-10">
-        {products?.map((item:any, index: any)=>( 
-                <Link to={`/detail/${item._id}`}>
-                    <Itemproduct itemproduct={item} key={index +1}/>
-                </Link>        
-        ))}
-        </div>
+    <div className='grid grid-cols-5 gap-10'>
+      {filteredProducts.length > 0 ? (
+        filteredProducts.map((item: any, index: any) => (
+          <Link to={`/detail/${item._id}`} key={index}>
+            <Itemproduct itemproduct={item} />
+          </Link>
+        ))
+      ) : (
+        <p className='w-[300px]  text-[18px] text-gray-600 font-semibold text-center'>Không có sản phẩm</p>
+      )}
     </div>
   )
 }
